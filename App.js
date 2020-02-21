11636f7270cc5a806067510942fe9bfbc995b79f
Tom Claudio <526473+gb96@users.noreply.github.com>
2020-02-21 14:39:04 +0800

    Import new components

--- App.js
+++ App.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import {View} from 'react-native';
 import Header from './components/Header';
+import Expenses from './components/Expenses';
 
 const App = () => {
-  return <Header />;
+  return (
+    <View>
+      <Header />
+      <Expenses />
+    </View>
+  );
 };
 
 export default App;
