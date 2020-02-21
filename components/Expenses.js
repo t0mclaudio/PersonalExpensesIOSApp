3b0b072ec3722d5752124c265944c9e3ee847fb3
Tom Claudio <526473+gb96@users.noreply.github.com>
2020-02-21 16:37:01 +0800

    improve layour

--- components/Expenses.js
+++ components/Expenses.js
@@ -1,13 +1,20 @@
-import React, { useState } from 'react';
-import { StyleSheet, View, Text, FlatList, TextInput, TouchableOpacity } from 'react-native';
-import { uuid } from 'uuidv4';
+import React, {useState} from 'react';
+import {
+  StyleSheet,
+  View,
+  Text,
+  FlatList,
+  TextInput,
+  TouchableOpacity,
+} from 'react-native';
+import {uuid} from 'uuidv4';
 import Icon from 'react-native-vector-icons/dist/FontAwesome';
 
 const Expenses = () => {
   const [expenses, setExpenses] = useState([
-    { id: uuid(), text: 'Milk', amount: 100 },
-    { id: uuid(), text: 'Eggs', amount: 100 },
-    { id: uuid(), text: 'Juice', amount: 100 },
+    {id: uuid(), date: new Date(), text: 'Milk', amount: 100},
+    {id: uuid(), date: new Date(), text: 'Eggs', amount: 100},
+    {id: uuid(), date: new Date(), text: 'Juice', amount: 100},
   ]);
 
   return (
@@ -20,34 +27,54 @@ const Expenses = () => {
       </TouchableOpacity>
       <FlatList
         data={expenses}
-        renderItem={({ item }) => <Expense item={item} />}
+        renderItem={({item}) => <Expense item={item} />}
       />
     </View>
   );
 };
 
-const Expense = ({ item }) => {
+const Expense = ({item}) => {
+  let date = `${item.date.getMonth() + 1}/${item.date.getDate()}`;
   return (
     <TouchableOpacity>
       <View style={styles.container}>
-        <Text style={styles.item}>{item.text} {`Php ${item.amount}`} </Text>
-        <Icon style={styles.icon} name="pencil" size={16} color="blue" />
-        <Icon style={styles.icon} name="remove" size={16} color="red" />
+        <View>
+          <Text style={styles.date}>{date}</Text>
+        </View>
+        <View>
+          <Text style={styles.text}>{item.text}</Text>
+        </View>
+        <View>
+          <Text style={styles.text}>{`₱${item.amount}`}</Text>
+        </View>
+        <View style={styles.icons}>
+          <Icon style={styles.icon} name="pencil" size={16} color="#3498db" />
+          <Icon style={styles.icon} name="remove" size={16} color="#e74c3c" />
+        </View>
       </View>
     </TouchableOpacity>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
+    marginTop: 5,
     flexDirection: 'row',
-    padding: 15,
+    padding: 12,
     backgroundColor: '#f8f8f8',
     borderBottomWidth: 1,
     borderColor: '#eee',
+    justifyContent: 'space-between',
   },
-  item: {
-    fontSize: 18,
+  icons: {
+    flexDirection: 'row',
+  },
+  date: {
+    color: '#bdc3c7',
+  },
+  text: {
+    color: '#34495e',
+    fontSize: 14,
   },
   ItemView: {
     flexDirection: 'row',
@@ -55,7 +82,9 @@ const styles = StyleSheet.create({
     color: 'white',
   },
   icon: {
-    padding: 5,
+    paddingTop: 3,
+    paddingBottom: 3,
+    paddingRight: 12,
   },
   input: {
     height: 60,
@@ -63,12 +92,12 @@ const styles = StyleSheet.create({
     margin: 5,
   },
   btn: {
-    backgroundColor: '#c2bad8',
+    backgroundColor: '#2ecc71',
     padding: 9,
     margin: 5,
   },
   btnText: {
-    color: 'darkslateblue',
+    color: 'white',
     fontSize: 20,
     textAlign: 'center',
   },
